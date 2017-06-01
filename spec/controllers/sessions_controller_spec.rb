657a057be0ea9f3f9f41f6625873578ef1aa1695
Kathy Van Stone <kvs@pobox.com>
2017-06-01 00:47:30 +0000

    Updated sessions controller to have old tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
+  let(:auth_environment) {
+    {uid: "12345", image_url: "http://test.com/iimage12345"}
+  }
+
+  let(:empty_session) { {} }
+  
+  describe "POST #create" do
+    context "with no matching profile" do
+      before {
+        @request.env["omniauth.auth"] = auth_environment
+        @response = post :create
+      }
+
+      it "puts an AuthUser into session" do
+        user = session[:user]
+        expect(user.uid).to eq auth_environment[:uid]
+      end
+
+      it "redirects to the creating a profile" do
+        expect(response).to redirect_to(new_profile_url)
+      end
+    end
+    
+    context "with matching profile"
+
+  end
 end
