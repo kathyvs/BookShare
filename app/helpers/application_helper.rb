9b62e326e3a1c1cd736ae89619e1f0529f0f42f9
Kathy Van Stone <kvs@pobox.com>
2017-09-25 22:45:59 -0400

    Updated to start with Bootstrap and fixed production issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
 
   include UserHelper
   
+  def button_link_to(name = nil, options = nil, html_options = nil, &block)
+    html_options ||= {}
+    html_options[:class => 'btn']
+    link_to(name + "TBD", options, html_options, &block)
+  end
 end
